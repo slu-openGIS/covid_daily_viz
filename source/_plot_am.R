89feeff3413ccc7fc5a86159a290b599383e5129
Christopher Prener <chris.prener@slu.edu>
2022-02-14 12:04:50 -0600

    build pm plots for 2022-02-14

--- source/_plot_am.R
+++ source/_plot_am.R
@@ -32,7 +32,7 @@ values <- list(
   date_breaks_long = "1 month",
   date_breaks_log = 50,
   date_breaks_facet = "4 months",
-  date_breaks_3days = "1 week", #  7 days
+  date_breaks_3days = "7 days", #   1 week
   x_angle = 25,
   
   county_log_max = 100000,
@@ -45,7 +45,7 @@ values <- list(
   county_log_y = .4,
   
   regional_current_x = 0,
-  state_current_x = -160,
+  state_current_x = -150,
   state_current_y = 7000,
   stl_peak_y = -1000,
   kc_peak_y = 1000,
