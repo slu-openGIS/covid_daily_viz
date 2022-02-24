7cf5d53bc49567b2b7a837d438acdd09747e68fc
Christopher Prener <chris.prener@slu.edu>
2022-02-23 18:42:37 -0600

    build pm plots for 2022-02-23

--- source/_plot_pm.R
+++ source/_plot_pm.R
@@ -66,9 +66,9 @@ source("source/functions/sequoia_theme.R")     # theme for plots and maps
 source("source/workflow/18_stl_zip_plots_v3.R")
 source("source/workflow/19_stl_individual_plots.R")
 
-# if (weekdays(date) %in% c("Saturday", "Sunday") == FALSE){
-#  source("source/workflow/20_stl_hospital_plots.R")  
-# }
+if (weekdays(date) %in% c("Saturday", "Sunday") == FALSE){
+  source("source/workflow/20_stl_hospital_plots.R")  
+}
 
 source("source/workflow/22_mo_deaths.R")
 source("source/workflow/23_vaccines.R")
